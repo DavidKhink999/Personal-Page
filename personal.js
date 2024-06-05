8746d3ddca705b31df6f59ccab057b8f8076d676
DavidKhink999 <pussandavid@gmail.com>
2024-06-05 10:15:37 +0400

    Add files via upload

---
+++ personal.js
@@ -0,0 +1,12 @@
+function scrollAbout(){
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' })
+}
+function scrollSkills(){
+    document.getElementById('skills').scrollIntoView({ behavior: 'smooth' })
+}
+function scrollExperience(){
+    document.getElementById('experience').scrollIntoView({ behavior: 'smooth' })
+}
+function scrollLink(){
+    document.getElementById('last').scrollIntoView({ behavior: 'smooth' })
+}
